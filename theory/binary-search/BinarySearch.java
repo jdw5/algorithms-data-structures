27fbdee1a05fc8504ba8fe094de8d6619e329129
jdw5 <joshuadwallace1@gmail.com>
2023-04-01 22:26:04 +1000

    Binary search

---
+++ theory/binary-search/BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+    
+    public static int binarySearch(int[] nums, int target) {
+        int l = 0, r = nums.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] < target) l = m + 1;
+            else r = m;
+        }
+        return l;
+    }
+}
---
+++ theory/binary-search/README.md
@@ -0,0 +1,3 @@
+# Binary Search
+- Binary search is used for when an array or list is presorted for finding an element.
+- Runs in **O(log n)** time where n is the size of the input array/list
